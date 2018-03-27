67cec35448f8af5fc1a16e44cd6db526cdff7e7e
Alex Orga <alexorga.dev@gmail.com>
2018-03-26 21:11:41 -0500

    created XHR request

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
-console.log('hi');
+
+
+const printToDom = (divId, string) => {
+    document.getElementById(divId).innerHTML = string;
+}
+
+const domString = (animalsArray) => {
+    let domString = '';
+    for (let i=0; i<animalsArray.length; i++){
+        domString += `<h1>${animalsArray[i].name}</h1>`;
+    };
+    printToDom("animal-holder", domString);
+}
+
+function executeThisCodeisXHRFails(){
+    console.log("something went wrong");
+}
+
+function executeThisCodeAfterFileLoaded(){
+    const data = JSON.parse(this.responseText);
+    domString(data.animals);
+}
+
+const startApplication = () => {
+    let myRequest = new XMLHttpRequest();
+    myRequest.addEventListener("load", executeThisCodeAfterFileLoaded);
+    myRequest.addEventListener("error", executeThisCodeisXHRFails);
+    myRequest.open("GET", "animals.json");
+    myRequest.send();
+}
+
+startApplication();
