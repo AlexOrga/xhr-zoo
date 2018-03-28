4684d2a6d2052946a1ee3eb49ac4a5d32d48fe44
Alex Orga <alexorga.dev@gmail.com>
2018-03-27 20:37:01 -0500

    added found button

--- main.js
+++ main.js
@@ -17,12 +17,65 @@ const domString = (animalsArray) => {
         domString +=    `<h3>${animal.number}</h3>`;
         domString +=    `<img class="animalImage" src=${animal.imageUrl}>`;
         domString +=    `<div class="button-container">`
-        domString +=        `<button class="escaped-btn">Escaped</button>`;
+        domString +=        `<button class="escaped">Escaped</button>`;
         domString +=    `</div>`
         domString += `</div>`;
     };
     printToDom("animal-holder", domString);
 }
+
+const addEscapedEventListeners = () => {
+    const escapedButtons = document.getElementsByClassName("escaped");
+    for (let i=0; i<escapedButtons.length; i++){
+        escapedButtons[i].addEventListener('click', animalEscaped)
+    }
+    // animalsEscaped();
+};
+
+const animalEscaped = (e) => {
+    const badAnimalButton = e.target.parentNode;
+    showCarnivores();
+    showVegetables();
+    showFoundButton(badAnimalButton);
+};
+
+const showFoundButton = (buttonContainer) => {
+    buttonContainer.innerHTML = `<button id="found">Found</button>`;
+    initializeFoundButton();
+};
+
+const initializeFoundButton = () => {
+    console.log('hi');
+    const foundButton = document.getElementById('found');
+    foundButton.addEventListener('click', () => {
+        const animals = document.getElementsByClassName('animal');
+        for (let m=0; m<animals.length; m++){
+            animals[m].children[3].innerHTML = `<button class="escaped">Escaped</button>`;
+            animals[m].classList.remove('green');
+            animals[m].classList.remove('red');
+        }
+        addEscapedEventListeners();
+    });
+};
+
+const showCarnivores = () => {
+    const carnivores = document.getElementsByClassName("carnivore");
+    for (let j=0; j<carnivores.length; j++){
+        carnivores[j].children[3].innerHTML = "";
+        carnivores[j].classList.add('red');
+    }
+};
+
+const showVegetables = () => {
+    const vegetables = document.getElementsByClassName('vegetable');
+    for (let k=0; k<vegetables.length; k++){
+        vegetables[k].children[3].innerHTML = `<button class="eatMe">EAT ME!</button>`;
+        vegetables[k].classList.add('green');
+    }
+};
+
+
+
 
 function executeThisCodeisXHRFails(){
     console.log("something went wrong");
@@ -31,6 +84,7 @@ function executeThisCodeisXHRFails(){
 function executeThisCodeAfterFileLoaded(){
     const data = JSON.parse(this.responseText);
     domString(data.animals);
+    addEscapedEventListeners();
 }
 
 const startApplication = () => {
